511383cae9460464aa77d398019928470a2b0f36
Mihajatiana <hei.mihajatiana@gmail.com>
2025-03-17 08:55:17 +0300

    feat: create EventList component to display all the events

---
+++ user/src/app/components/EventList.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { Event } from "@/types/event";
+import Link from "next/link";
+
+export default function EventList({ events }: { events: Event[] }) {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {events.map((event) => (
+        <div key={event.id} className="border p-4 rounded-lg shadow-md">
+          <Link href={`/event/${event.id}`}>
+            <h2 className="text-xl font-bold">{event.title}</h2>
+            <p className="text-gray-600">{event.description}</p>
+            <p className="mt-2">
+              <strong>Date :</strong> {event.date}
+            </p>
+            <p>
+              <strong>Lieu :</strong> {event.location}
+            </p>
+            <p>
+              <strong>Billets disponibles :</strong>{" "}
+              {event.tickets.reduce((sum, ticket) => sum + ticket.available, 0)}
+            </p>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+}

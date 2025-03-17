f2badd0def8211ebc6351d7e4cea6fa7ec9d3b87
Miharisoa Rakotonirina <hei.miharisoa@gmail.com>
2025-03-17 20:28:45 +0300

    Display all events using EventCardHome component

--- user/src/app/page.tsx
+++ user/src/app/page.tsx
@@ -1,7 +1,28 @@
+import Events from "../lib/events.json"
+import EventCardHome from "@/components/EventCardHome";
+
 export default function Home() {
   return (
-      <div className="landing_page h-[80vh]">
+      <>
+        <div className="landing_page h-[80vh] bg-red-700">
 
-      </div>
+        </div>
+
+          <section id="events" className="flex items-center flex-col">
+              <div>
+                  <h1>Search bar</h1>
+              </div>
+
+              <ul className="grid grid-cols-2 gap-8 px-8 mt-8">
+                  {Events.map((event) => {
+                      return (
+                          <li key={event.id}>
+                              <EventCardHome title={event.title} description={event.description} date={event.date} time={event.time} location={event.location} organizer={event.organizer} link={`/event/${event.id}`} />
+                          </li>
+                      )
+                  })}
+              </ul>
+          </section>
+      </>
   );
 }

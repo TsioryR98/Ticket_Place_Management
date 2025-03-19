c1fc2f30ee7c3f55bdcf30be42b973585ea53220
TsioryR98 <hei.tsiory.2@gmail.com>
2025-03-19 09:12:31 +0100

    refactor: divide SSR and CSR components for layout

--- user/src/app/layout.tsx
+++ user/src/app/layout.tsx
@@ -1,27 +1,27 @@
-"use client";
+/*
+NOTE:
+SSR only , if you have client components , redirect it into "@/components/ClientLayout"
+*/
+
+import type { Metadata } from "next";
 import "../styles/globals.css";
-import SideNavbar from "@/components/navbar/SideNavbar";
-import NavBar from "@/components/navbar/navbar";
-import LoginModal from "@/components/login/LoginModal";
-import { LoginModalContext } from "@/context/ModalContext";
-import { SessionProvider } from "next-auth/react";
+import ClientLayout from "@/components/ClientLayout";
+
+export const metadata: Metadata = {
+  title: "Tickify – Book Your Tickets for Unforgettable Events!",
+  description:
+    "Easily book tickets for concerts, festivals, shows, and more with Tickify. Secure your spot in just a few clicks and never miss out on amazing experiences!",
+};
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body className={`antialiased bg-white`}>
-        <LoginModalContext>
-          <SessionProvider>
-            <NavBar />
-            <SideNavbar />
-            <LoginModal />
-            {children}
-          </SessionProvider>
-        </LoginModalContext>
+      <body className="antialiased bg-white">
+        <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
   );

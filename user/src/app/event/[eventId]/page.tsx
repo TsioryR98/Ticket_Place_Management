e4319e26822cf24ace2030514d8dd6960ce787d4
Mihajatiana <hei.mihajatiana@gmail.com>
2025-03-20 14:49:56 +0300

    fix: add await for params

--- user/src/app/event/[eventId]/page.tsx
+++ user/src/app/event/[eventId]/page.tsx
@@ -8,7 +8,7 @@ export default async function EventPage({
 }: {
   params: { eventId: string };
 }) {
-  const { eventId } = params;
+  const { eventId } = await params;
 
   // Simuler une récupération asynchrone des données
   const event: Event | undefined = eventsData.find((e) => e.id === eventId);

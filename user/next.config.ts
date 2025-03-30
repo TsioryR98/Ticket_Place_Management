54d95e62c041b432c29ee1ab5a5fa4b36e0cb88d
Mihajatiana <hei.mihajatiana@gmail.com>
2025-03-30 10:56:38 +0300

    feat: add domains for the image url

--- user/next.config.ts
+++ user/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["photos.bandsintown.com", "assets.prod.bandsintown.com"],
+  },
 };
 
 export default nextConfig;

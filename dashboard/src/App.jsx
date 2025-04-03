7819fcda4b8707ae89adb98e075797349e1fc941
KiadyRafa <kiadyhenikhaja@gmail.com>
2025-04-03 04:16:09 +0300

    Ajout de provide et resourcer pour ticket

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -3,6 +3,19 @@ import { authProvider } from "./provider/AuthProvider";
 import { dataProvider } from "./provider/CombinedProvider";
 import { userList } from "./resource/UserResource";
 import SignIn from "./components/loginPage/LoginPage";
+import { TicketProvider } from "./provider/TicketProvider";
+
+//  dataProvider combiné
+const dataProvider = {
+  ...baseDataProvider,
+  tickets: ticketDataProvider,
+  getList: (resource, params) => {
+    return resource === 'tickets' 
+      ? ticketDataProvider.getList(resource, params)
+      : baseDataProvider.getList(resource, params);
+  },
+  // Ajoutez les autres méthodes si nécessaire
+};
 
 export const App = () => (
   <Admin
@@ -11,6 +24,14 @@ export const App = () => (
     authProvider={authProvider}
   >
     <Resource name="users" list={userList} />
+    <Resource 
+      name="tickets" 
+      list={ticketList}
+      edit={ticketEdit}
+      create={ticketCreate}
+    />
+    {/* Ajoutez d'autres ressources si nécessaire */}
   </Admin>
 );
+
 export default App;
